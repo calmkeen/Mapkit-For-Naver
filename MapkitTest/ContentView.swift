88c4087b11d41cf5cbd9c44d4005ce82361d93a6
calmkeen <iyunseong159@gmail.com>
2022-04-06 22:35:00 +0900

    Initial Commit

---
+++ MapkitTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MapkitTest
+//
+//  Created by calmkeen on 2022/04/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MapkitTest/MapkitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapkitTestApp.swift
+//  MapkitTest
+//
+//  Created by calmkeen on 2022/04/06.
+//
+
+import SwiftUI
+
+@main
+struct MapkitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
